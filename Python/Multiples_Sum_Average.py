096b8202e47e45d42777845222c662c76ebaaf7c
David Wukelic <wukebb24@hotmail.com>
2018-08-26 19:29:22 -0700

    Multiples Part I

---
+++ Python/Multiples_Sum_Average.py
@@ -0,0 +1,9 @@
+# Multiplies, Sum, Average Assignment  
+# Created By: David Wukelic
+# Utilizing tab spacing
+
+# Multiples Part I
+# Print odd numbers between 1 - 1000
+for count in range (1, 1001, 2):
+    print count
+

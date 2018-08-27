966664c0ce6a5ea8b24916575de6130c0ca63073
David Wukelic <wukebb24@hotmail.com>
2018-08-26 19:31:02 -0700

    Multiples Part II

--- Python/Multiples_Sum_Average.py
+++ Python/Multiples_Sum_Average.py
@@ -3,7 +3,11 @@
 # Utilizing tab spacing
 
 # Multiples Part I
-# Print odd numbers between 1 - 1000
+# Print odd numbers between 1 - 1,000
 for count in range (1, 1001, 2):
     print count
 
+# Multiples Part II
+# Print the multiples of 5 between 5 - 1,000,000
+for count in range (5, 1000001, 5):
+    print count

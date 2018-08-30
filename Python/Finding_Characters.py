28e5d5288c9cb2e54d21ac9ec317d7dd5865ae4c
David Wukelic <wukebb24@hotmail.com>
2018-08-29 18:35:35 -0700

    layout

--- Python/Finding_Characters.py
+++ Python/Finding_Characters.py
@@ -1,3 +1,15 @@
 # Finding Characters Assignment  
 # Created By: David Wukelic
 # Utilizing tab spacing
+
+'''
+Write a program that takes a list of strings and a string containing a single 
+character, and prints a new list of all the strings containing that character.
+
+'''
+
+# input
+word_list = ['hello','world','my','name','is','Anna']
+char = 'o'
+# output
+new_list = ['hello','world']

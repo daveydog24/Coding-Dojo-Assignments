5e366edcc3ba5468cd5f532c4c075c9558abba4d
David Wukelic <wukebb24@hotmail.com>
2018-08-29 22:04:20 -0700

    comments and validations

--- Python/Finding_Characters.py
+++ Python/Finding_Characters.py
@@ -16,14 +16,19 @@ new_list = ['hello','world']
 
 '''
 
+# takes a character and checks to see if it is within each value of the given array and returns all the words that contain the character
 def Find_Character(char, arr):
-    new_list = []
-    for x in arr:
-        for c in x:
-            if c == char:
-                new_list.append(x)
-                break
-    print new_list
+    if len > 0:
+        new_list = []
+        for x in arr:
+            for c in x:
+                if c == char:
+                    new_list.append(x)
+                    break
+        print new_list
+    else:
+        return []
+        
 word_list = ['hello','world','my','name','is','Anna']
 char = 'o'
 

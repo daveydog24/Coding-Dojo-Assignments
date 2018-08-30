3a403000b33996a4490b1997927f174332dfde63
David Wukelic <wukebb24@hotmail.com>
2018-08-29 21:59:28 -0700

    final touches

--- Python/Finding_Characters.py
+++ Python/Finding_Characters.py
@@ -17,8 +17,14 @@ new_list = ['hello','world']
 '''
 
 def Find_Character(char, arr):
+    new_list = []
     for x in arr:
-        print x;
-        print char
+        for c in x:
+            if c == char:
+                new_list.append(x)
+                break
+    print new_list
+word_list = ['hello','world','my','name','is','Anna']
+char = 'o'
 
 Find_Character(char, word_list)

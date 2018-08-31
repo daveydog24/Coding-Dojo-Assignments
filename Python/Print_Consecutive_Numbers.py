55881486d2e5c7a7857f014d4f56ca7319dd833b
David Wukelic <wukebb24@hotmail.com>
2018-08-30 17:29:31 -0700

    refined algo

--- Python/Print_Consecutive_Numbers.py
+++ Python/Print_Consecutive_Numbers.py
@@ -19,39 +19,18 @@ Output Format
 123 
 
 '''
+    n = 3
+    sum = 1
+    counter = 1
 
-import math
-if __name__ == '__main__':
-    n = int(input())
-    sum = 0
-    counter = 0
-    while n > 0:
-        if counter == 0:
-            sum += 1
-        elif counter == 1:
-            sum += math.floor(1.1 * (10**counter))
-        elif counter == 2:
-            sum += math.floor(1.1 * (10**counter)) + 1
-        elif counter == 3:
-            sum += math.floor(1.1 * (10**counter)) + 11
-        elif counter == 4:
-            sum += math.floor(1.1 * (10**counter)) + 111
-        elif counter == 5:
-            sum += math.floor(1.1 * (10**counter)) + 1111
-        elif counter == 6:
-            sum += math.floor(1.1 * (10**counter)) + 11111
-        elif counter == 7:
-            sum += math.floor(1.1 * (10**counter)) + 111111
-        elif counter == 8:
-            sum += math.floor(1.1 * (10**counter)) + 1111111
-        elif counter == 9:
-            sum += math.floor(1.1 * (10**counter)) + 11111111
-        elif counter == 10:
-            sum += math.floor(1.1 * (10**counter)) + 111111111
-        elif counter == 11:
-            sum += math.floor(1.1 * (10**counter)) + 1111111111
-        else:
-            sum += math.floor(1.1 * (10**counter)) + 11111111111
-        counter += 1
-        n -= 1                
+    while counter < n:
+        multiplier = counter
+        temp_sum = 0
+
+        while multiplier >= 0:
+            temp_sum += (10**multiplier)
+            multiplier -= 1
+
+        sum += temp_sum
+        counter += 1   
     print (sum)

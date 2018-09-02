ce0c40c5e5ded4b71d1f30802f511d1b676ae8b9
David Wukelic <wukebb24@hotmail.com>
2018-09-02 13:37:52 -0700

    first attempt

--- Python/Hacker_Rank/Runner_Up.py
+++ Python/Hacker_Rank/Runner_Up.py
@@ -5,24 +5,24 @@ Utilizing tab spacing
 
 
 '''
-    n = 5
-    arr = map(int, input().split())
-    first = None
-    second = None
-    
-    for value in arr:
-        print value
-        if not first:
-            first = value
-        if value >= first:
-            first = value
-        else:
-            if not second:
-                second = value
-            if value <= first and value >= second:
-                second = value
-            
-        # if value == first and value != second:
-        #     second = value
+n = 5
+arr = map(int, input().split())
+first = None
+second = None
+
+for value in arr:
+    print value
+    if not first:
+        first = value
+    if value >= first:
+        first = value
+    else:
+        if not second:
+            second = value
+        if value <= first and value >= second:
+            second = value
         
-    print (second)
+    # if value == first and value != second:
+    #     second = value
+    
+print (second)

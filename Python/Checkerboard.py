9d7d233cc88957810cec125ac9b9aa8c185d3ebe
David Wukelic <wukebb24@hotmail.com>
2018-09-03 15:23:57 -0700

    finished

--- Python/Checkerboard.py
+++ Python/Checkerboard.py
@@ -3,3 +3,20 @@ Checkerboard Assignment
 Created By: David Wukelic
 Utilizing tab spacing
 '''
+
+rows = 15
+stars_in_row = 30
+
+while rows > 0:
+    printed = ''
+    stars = stars_in_row
+    if rows % 2 == 0:
+        while stars > 0:
+            printed += "* "
+            stars -= 1
+    else:
+        while stars > 0:
+            printed += " *"
+            stars -= 1
+    rows -= 1
+    print printed

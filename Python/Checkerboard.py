1d6f9341c4418dc575af0974466880cffba4c4b6
David Wukelic <wukebb24@hotmail.com>
2018-09-03 15:35:44 -0700

    added flare and ability to customize output

--- Python/Checkerboard.py
+++ Python/Checkerboard.py
@@ -4,19 +4,21 @@ Created By: David Wukelic
 Utilizing tab spacing
 '''
 
-rows = 15
-stars_in_row = 30
 
-while rows > 0:
-    printed = ''
-    stars = stars_in_row
-    if rows % 2 == 0:
-        while stars > 0:
-            printed += "* "
-            stars -= 1
-    else:
-        while stars > 0:
-            printed += " *"
-            stars -= 1
-    rows -= 1
-    print printed
+def Print_Symbol(rows, stars_in_row, symbol):
+    while rows > 0:
+        printed = ''
+        stars = stars_in_row
+        if rows % 2 == 0:
+            while stars > 0:
+                printed += "{} ".format(symbol)
+                stars -= 1
+        else:
+            while stars > 0:
+                printed += " {}".format(symbol)
+                stars -= 1
+        rows -= 1
+        print printed
+
+# define how many rows and stars you want 
+Print_Symbol(10, 10, '**')

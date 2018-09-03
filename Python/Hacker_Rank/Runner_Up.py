ecb3df9e5ed08d6485499266c89f341ff917f782
David Wukelic <wukebb24@hotmail.com>
2018-09-02 21:27:49 -0700

    changes

--- Python/Hacker_Rank/Runner_Up.py
+++ Python/Hacker_Rank/Runner_Up.py
@@ -3,27 +3,45 @@ Runner Up Assignment
 Created By: David Wukelic
 Utilizing tab spacing
 
+n = 5
+arr = [-10, 0, 10]
+highest = None
+second_highest = None
+
+# loops through each value in the array
+for value in arr:
+
+    # if its the first time through it stores the first variable as the highest number
+    if not highest:
+        highest = value
+
+    # checks value and if they are higher than the stored highest variable it stores the new value
+    if value > highest:
+        second_highest = highest
+        highest = value
+
+print (second_highest)
+print (highest)
 
 '''
-# n = 5
-# arr = map(int, input().split())
-arr = [6, 6, 6, 6, 6, 5]
-first = None
-second = None
+
+n = 5
+arr = [-10, 0, 10]
+highest = None
+second_highest = None
 
 for value in arr:
-    if not first:
-        first = value
-    if value > first:
-        first = value
-    elif not second:
-        if value != first:
-            second = value
-    if value < 0:
-        if value <= second and value != first:
-            second = value
+    if not highest:
+        print "in non highest loop", value
+        highest = value
     else:
-        if value >= second and value != first:
-            second = value
-    
-print (second)
+        if value > highest:
+            second_highest = highest
+            highest = value
+        else:
+            if not second_highest:
+                second_highest = value
+            if value > second_highest and value != highest:
+                second_highest = value
+
+print (second_highest)

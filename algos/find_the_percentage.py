519ed61f668dbcbb34b95dceaaf6cb5f0d43a31d
David Wukelic <wukebb24@hotmail.com>
2018-09-25 22:33:25 -0700

    header

--- algos/find_the_percentage.py
+++ algos/find_the_percentage.py
@@ -1,11 +1,15 @@
-
 '''
-You have a record of students. Each record contains the student's name, 
-and their percent marks in Maths, Physics and Chemistry. The marks can be 
-floating values. The user enters some integer followed by the names and marks 
-for students. You are required to save the record in a dictionary data type. 
-The user then enters a student's name. Output the average percentage marks 
-obtained by that student, correct to two decimal places.
+Compare List Assignment  
+Created By: David Wukelic
+Utilizing tab spacing
+
+Assignment Info
+    You have a record of students. Each record contains the student's name, 
+    and their percent marks in Maths, Physics and Chemistry. The marks can be 
+    floating values. The user enters some integer followed by the names and marks 
+    for students. You are required to save the record in a dictionary data type. 
+    The user then enters a student's name. Output the average percentage marks 
+    obtained by that student, correct to two decimal places.
 
 Input Format
     The first line contains the integer , the number of students. 

0c8df7f6f0e069e2a8cc5238bf63bae42d25c7a8
David Wukelic <wukebb24@hotmail.com>
2018-09-30 22:23:03 -0700

    added function in prototype to check height of tree

--- algos/modal_testing/studying/binarySearchTree.js
+++ algos/modal_testing/studying/binarySearchTree.js
@@ -28,3 +28,12 @@ BinarySearchTree.prototype.isBST = function (node){
     return true;  
 }
 
+BinarySearchTree.prototype.height = function (node){
+    if(!node) {
+        return 0;
+    }
+    var leftHeight = height(node.left);
+    var rightHeight = height(node.right);
+ 
+    return Math.max(leftHeight, rightHeight) + 1;
+}

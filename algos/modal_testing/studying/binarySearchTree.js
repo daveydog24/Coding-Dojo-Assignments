635a05b32dfa4f60959ab0cc1893092b73e1b932
David Wukelic <wukebb24@hotmail.com>
2018-09-30 22:22:38 -0700

    added function in prototype to add value to tree

--- algos/modal_testing/studying/binarySearchTree.js
+++ algos/modal_testing/studying/binarySearchTree.js
@@ -8,37 +8,23 @@ function BinarySearchTree(){
     this.root = null;
 }
 
-BinarySearchTree.prototype.push = function(val){
-    var root = this.root;
- 
-    if(!root){
-       this.root = new Node(val);
-       return;
+BinarySearchTree.prototype.isBST = function (node){
+    if(!node){
+        return true; 
     }
  
-    var currentNode = root;
-    var newNode = new Node(val); 
+    if(node.left != null && node.left.value > node.value){ 
+        return false;
+    }
  
-    while(currentNode){
-       if(val < currentNode.value){
-           if(!currentNode.left){
-              currentNode.left = newNode;
-              break;
-           }
-           else{
-              currentNode = currentNode.left;
-         }
-      }
-      else{
-          if(!currentNode.right){
-             currentNode.right = newNode;
-             break;
-          }
-          else{
-             currentNode = currentNode.right;
-          }
-      }
-   }
+    if(node.right !=null && node.right.value < node.value) {
+        return false;
+    }
  
- }
-    
+    if(!isBST(node.left) || !isBST(node.right)) {
+        return false;
+    }
+ 
+    return true;  
+}
+

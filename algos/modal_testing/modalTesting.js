2059edf0e38a36d1e2b529b8e0768e93ac1c85eb
David Wukelic <wukebb24@hotmail.com>
2018-09-30 21:22:03 -0700

    moved js to external sheet

---
+++ algos/modal_testing/modalTesting.js
@@ -0,0 +1,10 @@
+function openModal(name){
+    itemContent = document.getElementById(name);
+    itemContent.style.display = "block";
+}
+
+function closeModal(name){
+    itemContent = document.getElementById(name);
+    itemContent.style.display = "none";
+}
+

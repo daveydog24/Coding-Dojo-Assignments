5c5042cc0a69dd9ff442b180578404322b6f248e
David Wukelic <wukebb24@hotmail.com>
2018-09-30 22:25:53 -0700

    added function to check if its a BST

--- algos/modal_testing/studying/binarySearchTree.js
+++ algos/modal_testing/studying/binarySearchTree.js
@@ -1,35 +1,35 @@
-function Node(val){
+function Node(val) {
     this.value = val;
     this.left = null;
     this.right = null;
 }
 
-function BinarySearchTree(){
+function BinarySearchTree() {
     this.root = null;
 }
 
-BinarySearchTree.prototype.isBST = function (node){
-    if(!node){
+BinarySearchTree.prototype.isBST = function (node) {
+    if (!node) {
         return true; 
     }
  
-    if(node.left != null && node.left.value > node.value){ 
+    if (node.left != null && node.left.value > node.value) { 
         return false;
     }
  
-    if(node.right !=null && node.right.value < node.value) {
+    if (node.right !=null && node.right.value < node.value) {
         return false;
     }
  
-    if(!isBST(node.left) || !isBST(node.right)) {
+    if (!isBST(node.left) || !isBST(node.right)) {
         return false;
     }
  
     return true;  
 }
 
-BinarySearchTree.prototype.height = function (node){
-    if(!node) {
+BinarySearchTree.prototype.height = function (node) {
+    if (!node) {
         return 0;
     }
     var leftHeight = height(node.left);
@@ -37,3 +37,34 @@ BinarySearchTree.prototype.height = function (node){
  
     return Math.max(leftHeight, rightHeight) + 1;
 }
+
+BinarySearchTree.prototype.push = function(val) {
+    var root = this.root;
+ 
+    if (!root) {
+       this.root = new Node(val);
+       return;
+    }
+ 
+    var currentNode = root;
+    var newNode = new Node(val); 
+ 
+    while(currentNode) {
+        if (val < currentNode.value) {
+            if (!currentNode.left) {
+                currentNode.left = newNode;
+                break;
+            } else {
+                currentNode = currentNode.left;
+            }
+        } else {
+            if (!currentNode.right) {
+                currentNode.right = newNode;
+                break;
+            } else {
+                currentNode = currentNode.right;
+            }
+        }
+    }
+}
+    

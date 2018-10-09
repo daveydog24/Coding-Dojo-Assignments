5c546503c6ee49a08659b7e9375b5a4e01528234
David Wukelic <david.wukelic@gmail.com>
2018-10-08 23:06:41 -0700

    switched main.js to reflect movies

--- algos/modal_testing/main.js
+++ algos/modal_testing/main.js
@@ -1,38 +1,45 @@
 // add more data here for more items
 data = [
     {
-        name: "iPhone",
-        url: "https://amp.businessinsider.com/images/59c29dcb2488492b395ca43e-960-623.jpg",
+        title: "iPhone",
+        image: "https://amp.businessinsider.com/images/59c29dcb2488492b395ca43e-960-623.jpg",,
+        description: " Movie Description would normally go here"
     },
     {
-        name: "Macbook",
-        url: "https://cnet4.cbsistatic.com/img/s7OzxR8Y3xxOlegTpRQW3CuZPuk=/830x467/2017/06/06/349b7e65-a855-44f0-ae0a-6610ebca0809/apple-macbook-pro-12-inch-2017-4181.jpg"
+        title: "Macbook",
+        image: "https://cnet4.cbsistatic.com/img/s7OzxR8Y3xxOlegTpRQW3CuZPuk=/830x467/2017/06/06/349b7e65-a855-44f0-ae0a-6610ebca0809/apple-macbook-pro-12-inch-2017-4181.jpg",
+        description: " Movie Description would normally go here"
     },
     {
-        name: "Car",
-        url: "https://imgct2.aeplcdn.com/img/800x600/car-data/big/honda-city-image-9579.png?v=29"
+        title: "Car",
+        image: "https://imgct2.aeplcdn.com/img/800x600/car-data/big/honda-city-image-9579.png?v=29",
+        description: " Movie Description would normally go here"
     },
     {
-        name: "Television",
-        url: "http://imshopping.rediff.com/imgshop/300-400/shopping/pixs/25424/b/bush32._bush-32-inch-curved-led-tv.jpg",
+        title: "Television",
+        image: "http://imshopping.rediff.com/imgshop/300-400/shopping/pixs/25424/b/bush32._bush-32-inch-curved-led-tv.jpg",,
+        description: " Movie Description would normally go here"
     },
     {
-        name: "Bike",
-        url: "https://imgd.aeplcdn.com//1056x594//bw/models/tvs-apache-rtr-160-4v.jpg?20180813151835"
+        title: "Bike",
+        description: " Movie Description would normally go here"
     },
     {
-        name: "BABE <3",
+        title: "BABE <3",
         // movie: "babe.move",
+        title: ""
+        image: "https://imgd.aeplcdn.com//1056x594//bw/models/tvs-apache-rtr-160-4v.jpg?20180813151835",
+        description: " Movie Description would normally go here"
     }
 ]
 
-function openModal(name){
-    itemContent = document.getElementById(name);
+function openModal(title){
+    itemContent = document.getElementById(title);
     itemContent.style.display = "block";
 }
 
-function closeModal(name){
-    itemContent = document.getElementById(name);
+function closeModal(title){
+    itemContent = document.getElementById(title);
     itemContent.style.display = "none";
 }
 
